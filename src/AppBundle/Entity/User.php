44f27e478da6ab24642ed91884747f539d1c6427
Your name <dydokamil@gmail.com>
2017-04-23 13:45:55 +0200

    Nadanie typu UUID kolumnie id

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -15,9 +15,9 @@ class User
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="id", type="guid")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\GeneratedValue(strategy="UUID")
      */
     private $id;
 
